ef9821df3ce92691f25d41c62b9d233239b580cf
Tariq Abdullah <tabdullah1215@gmail.com>
2025-01-23 10:30:44 -0500

    service worker with no or low caching

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,24 +8,18 @@ clientsClaim();
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         fetch(event.request).catch(() => {
-            // Optionally, add fallback logic if the network request fails
             console.error('Network fetch failed for:', event.request.url);
             return new Response('Network fetch failed.');
         })
     );
 });
 
-// This allows the web app to trigger skipWaiting via
-// registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
         self.skipWaiting();
     }
 });
 
-// Ensure immediate activation of the service worker
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim());
 });
-
-// Any other custom service worker logic can go here.
